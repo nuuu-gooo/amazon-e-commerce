f7b2f899ca685262d9b97de86dc548395e03a138
Nugzar Marjanidze <nugzarmarjanidze@MBP-von-Nugzar.home>
2024-01-27 14:58:54 +0200

    styled category products

--- src/views/DynamicPages/CategoryProducts/CategoryProducts.tsx
+++ src/views/DynamicPages/CategoryProducts/CategoryProducts.tsx
@@ -1,5 +1,6 @@
 import { axiosInstance } from "@src/utils/publicAxios";
 import React, { useEffect, useState } from "react";
+import { Card } from "antd";
 import { useParams } from "react-router-dom";
 export const CategoryProducts = () => {
   const [products, setProducts] = useState<TCategoryProducts[]>([]);
@@ -14,15 +15,35 @@ export const CategoryProducts = () => {
   useEffect(() => {
     fetchCategoryProducts();
   }, [productCategoryId]);
-  return (
-    <div>
-      <h1>{productCategoryId}</h1>
 
-      {products.length === 0 ? (
-        <p>Products not Found</p>
-      ) : (
-        products.map((product) => <h1>{product.title}</h1>)
-      )}
+  const { Meta } = Card;
+  return (
+    <div className="flex justify-between items-center p-9">
+      <div className="left flex justify-center items-center">
+        <p>Filters</p>
+      </div>
+
+      <div className="right grid grid-cols-2 gap-2">
+        {products.length === 0 ? (
+          <p>Products not Found</p>
+        ) : (
+          products.map((product) => {
+            return (
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src={product.image} />}
+              >
+                <Meta title={product.title} description={product.description} />
+                <p className="mt-[12%]">{product.price}$</p>
+              </Card>
+            );
+          })
+        )}
+      </div>
+      <div className="right">
+        <p className="text-[transparent]">Hello</p>
+      </div>
     </div>
   );
 };
