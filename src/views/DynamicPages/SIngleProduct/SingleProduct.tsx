9dddaa8ce5d6223481090657ba43defa8184e440
Nugzar Marjanidze <nugzarmarjanidze@MBP-von-Nugzar.home>
2024-02-02 16:44:09 +0200

    made markup for single page view

--- src/views/DynamicPages/SIngleProduct/SingleProduct.tsx
+++ src/views/DynamicPages/SIngleProduct/SingleProduct.tsx
@@ -30,9 +30,42 @@ export const SingleProduct = () => {
       {loading ? <Loader /> : ""}
       {singleProduct?.map((product) => {
         return (
-          <div>
-            <h1>{product.title}</h1>
-            <p>{product.price}$</p>
+          <div className="md:flex p-9 justify-between items-center">
+            <div className="left flex items-center flex-col">
+              <img
+                className="mx-auto h-[250px] w-[250px] cursor-pointer object-fit"
+                src={product.image}
+                alt="product-img"
+              />
+            </div>
+            <div className="middle flex flex-col items-start">
+              <h1>{product.title}</h1>
+              <hr className="border-solid border-black w-full mt-1" />
+              <div className="price flex items-center gap-3 pt-[20px]">
+                <p>Price:</p>
+                <p className="text-2xl text-red-700">{product.price} $</p>
+              </div>
+              <div className="product-description flex flex-col items-start mt-3">
+                <h4 className="mb-2">About this Item:</h4>
+                <p>{product.description}</p>
+              </div>
+            </div>
+            <div className="right  inline-flex flex-col items-start border-solid border-black p-6 rounded-sm">
+              <div className="price flex items-start">
+                <p className="text-sm font-medium">$</p>
+                <p className="text-3xl  font-medium">{product.price}</p>
+              </div>
+              <p className=" text-[#565959] ">
+                No Import Fees Deposit & <br /> $16.78 Shipping to Greece
+              </p>
+              <p className="mt-3">
+                Delivery{" "}
+                <span className="font-medium">
+                  Wednesday, February 28. <br />
+                </span>{" "}
+                Order within <span className="text-[green]">7 hrs 29 mins</span>
+              </p>
+            </div>
           </div>
         );
       })}
