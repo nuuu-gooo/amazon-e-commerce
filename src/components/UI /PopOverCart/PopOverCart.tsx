01792dd6ebf99d7f56ab28af1553180027dc7c53
Nugzar Marjanidze <nugzarmarjanidze@MBP-von-Nugzar.home>
2024-02-12 14:32:54 +0200

    latest changes

--- src/components/UI /PopOverCart/PopOverCart.tsx
+++ src/components/UI /PopOverCart/PopOverCart.tsx
@@ -2,9 +2,12 @@ import React, { useContext } from "react";
 import { GlobalContext } from "@src/providers/GlobalProvider";
 import { useNavigate } from "react-router-dom";
 import { Button } from "antd";
+import { AuthContext } from "@src/providers/Auth/AuthContext";
+import { authStage_EUNM } from "@src/ENUMS/Enums";
 
 export const PopOverCart = () => {
   const { allCartProducts, totalCartPrice } = useContext(GlobalContext);
+  const { authStage } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
     <div className="p-3 flex flex-col ">
@@ -29,20 +32,31 @@ export const PopOverCart = () => {
           );
         })
       )}
-      <div className="flex justify-center items-center flex-col mt-3">
-        <p>
-          Total:
-          <span className="text-[red] text-2xl">${totalCartPrice}</span>
-        </p>
+      {authStage === authStage_EUNM.AUTHORIZED ? (
+        <div className="flex justify-center items-center flex-col mt-3">
+          <p>
+            Total:
+            <span className="text-[red] text-2xl">${totalCartPrice}</span>
+          </p>
+          <button
+            onClick={() => navigate("/checkout")}
+            className="mt-3 cursor-pointer px-2.5 rounded-md  font-titleFont font-sm text-base bg-gradient-to-tr from-yellow-400
+to-yellow-200 border border-yellow-500 hover:border-yellow-700 hover:from-yellow-300 to hover:to-yellow-400 
+active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200"
+          >
+            Checkout
+          </button>
+        </div>
+      ) : (
         <button
-          onClick={() => navigate("/checkout")}
+          onClick={() => navigate("/login")}
           className="mt-3 cursor-pointer px-2.5 rounded-md  font-titleFont font-sm text-base bg-gradient-to-tr from-yellow-400
- to-yellow-200 border border-yellow-500 hover:border-yellow-700 hover:from-yellow-300 to hover:to-yellow-400 
- active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200"
+to-yellow-200 border border-yellow-500 hover:border-yellow-700 hover:from-yellow-300 to hover:to-yellow-400 
+active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200"
         >
-          Checkout
+          Register
         </button>
-      </div>
+      )}
     </div>
   );
 };
