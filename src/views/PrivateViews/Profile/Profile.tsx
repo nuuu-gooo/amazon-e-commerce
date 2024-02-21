6358a571b6ccac97d7834d11bd4e7eb3640ab022
Nugzar Marjanidze <nugzarmarjanidze@MacBook-Pro-von-Nugzar.local>
2024-02-21 16:52:39 +0200

    latest changes

--- src/views/PrivateViews/Profile/Profile.tsx
+++ src/views/PrivateViews/Profile/Profile.tsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 export const Profile = () => {
   document.title = "Amazon | Profile";
   return (
-    <div className="flex  flex-col w-[70%] m-auto p-3">
-      <h2>Your Profile</h2>
-      <div className=" inline-grid grid-cols-2 mt-2">
+    <div className="flex  flex-col justify-center items-center p-3">
+      <h3>Your Profile</h3>
+      <div className=" inline-grid grid-cols-2 mt-2 gap-3 ">
         {profile_elements.map((element) => {
           return (
             <Link
               className="no-underline w-full text-black hover:underline  "
               to={element.pageLink}
             >
-              <div className="inline-flex items-center border border-solid border-black p-4 justify-start rounded-sm  h-16 hover:opacity-50 cursor-pointer shadow-sm shadow-black">
+              <div className="inline-flex items-center border border-solid border-black p-4 justify-start rounded-sm  h-16 hover:opacity-50 cursor-pointer shadow-sm shadow-black min-w-[170px] max-w-[200px] ">
                 <img className="w-[10%]" src={element.imgSrc} alt="" />
-                <div className="flex-col items-center ml-3 ">
+                <div className="flex-col items-center ml-3   ">
                   <h4>{element.name}</h4>
                 </div>
               </div>
